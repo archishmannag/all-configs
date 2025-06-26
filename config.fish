fc77e5177049cd43cb63c8b9ad23edb74d0c1dc9
Archishman Nag <50835593+archishmannag@users.noreply.github.com>
2025-06-27 00:28:12 +0530

    Hide path

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
-    set -Ux STARSHIP_CONFIG /run/media/archishman/D-Drive/starship.toml
+    set -Ux STARSHIP_CONFIG ./starship.toml
     starship init fish | source
     alias ls=eza
     alias cat=bat
