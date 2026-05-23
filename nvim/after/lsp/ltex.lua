ba7d8de9867a7ad8f092dc6b2824d8fdd20d48d5
Archishman Nag <nag.archishman@gmail.com>
2026-05-23 19:19:12 +0530

    Update nvim config

--- nvim/after/lsp/ltex.lua
+++ nvim/after/lsp/ltex.lua
@@ -1,6 +1,10 @@
 local helper = require("nagar.config.helper")
 
 return {
+    cmd = {
+        'JAVA_OPTS="-Djdk.xml.totalEntitySizeLimit=0"',
+        "ltex-ls",
+    },
     root_markers = {
         ".bib",
         ".tex",
